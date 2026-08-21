b5326c6ce1113a663205a92077d1c658a7a44eb2
AlexPlay1 <42866177+AlexPlay1@users.noreply.github.com>
2026-08-21 12:43:45 +0300

    feat: add containerfile, app.py and requirements.txt

---
+++ Containerfile
@@ -0,0 +1,15 @@
+# Build stage
+FROM python:alpine AS builder
+COPY requirements.txt /
+RUN pip install --disable-pip-version-check --root-user-action ignore -r requirements.txt --target /packages
+
+# Runtime stage
+FROM gcr.io/distroless/python3:nonroot
+COPY --from=builder /packages /packages
+WORKDIR /app
+COPY app.py /app
+ENV PYTHONPATH=/packages
+
+EXPOSE 3000
+
+CMD ["-m", "gunicorn", "--bind", " 0.0.0.0:3000", "app:app"]
---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, jsonify
+import docker
+import requests
+import os
+import datetime
+
+app = Flask(__name__)
+client = docker.from_env()
+
+def validate_turnstile(token, secret, remoteip=None):
+  url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify'
+
+  data = {
+    'secret': secret,
+    'response': token
+  }
+
+  if remoteip:
+    data['remoteip'] = remoteip
+
+  try:
+    response = requests.post(url, data=data, timeout=10)
+    response.raise_for_status()
+    return response.json()
+  except requests.RequestException as e:
+    print(f"Turnstile validation error: {e}")
+    return {'success': False, 'error-codes': ['internal-error']}
+
+@app.route('/restart', methods=['GET', 'POST'])
+def restart_container_post():
+  if request.form.get('password') != os.environ['ACCESS_CODE']: return jsonify({'status': 'error', 'description': 'Access code is invalid'})
+  token = request.form.get('cf-turnstile-response')
+  remoteip = request.headers.get('CF-Connecting-IP') or \
+              request.headers.get('X-Forwarded-For') or \
+              request.remote_addr
+
+  validation = validate_turnstile(token, os.environ['TURNSTILE_SECRET'], remoteip)
+
+  if validation['success']:
+    print(f'[{datetime.datetime.now(datetime.timezone.utc)} UTC+0] Turnstile validation success! Remote IP: {remoteip}. Restarting the container')
+    print(client.containers.get('tor').restart())
+    return jsonify({'status': 'success', 'message': 'Container restarted successfully'})
+  else:
+    print(f'[{datetime.datetime.now(datetime.timezone.utc)} UTC+0] Turnstile validation failed! Remote IP: {remoteip}. Errors: {validation['error-codes']}')
+    return jsonify({
+        'status': 'error',
+        'message': 'Verification failed',
+        'errors': validation['error-codes']
+    }), 400
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
